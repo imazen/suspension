8055f99e47b5a3127f37b56cf6318cfe605d1e27
Jo Hund <jhund@clearcove.ca>
2013-09-10 17:08:22 -0700

    Removed :escaped_character token because we can't make it work reliably. Use HTML entities instead for @ and %.

--- lib/suspension/repositext_tokens.rb
+++ lib/suspension/repositext_tokens.rb
@@ -4,7 +4,6 @@ module Suspension
 
   REPOSITEXT_TOKENS = [
     Token.new(:record, /^\^\^\^\s*?(\{:(\\\}|[^\}]+)\})?\s*?\n/),
-    Token.new(:escaped_character, /\\[\\@%]/, true), # Should permit backslash-escaped characters
     Token.new(:subtitle_mark, /@/),
     Token.new(:subtitle_mark, /%/)
   ]
