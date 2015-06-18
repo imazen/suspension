f7d4e331d3b0441c168f9ff5192f9b3ecff22a1f
Jo Hund <jhund@clearcove.ca>
2015-06-17 17:27:22 -0700

    Updated StringComparer to handle multibyte characters

--- lib/suspension/string_comparer.rb
+++ lib/suspension/string_comparer.rb
@@ -13,13 +13,15 @@ module Suspension
         return []
       else
         diffs = Suspension::DiffAlgorithm.new.call(string_1, string_2)
-        deltas = []
-        char_num = 0
-        line_num = 1
-        excerpt_window = 20
         # Add context information to diffs
-        deltas = diffs.map { |diff|
-          begin
+        begin
+          deltas = []
+          char_num = 0
+          line_num = 1
+          excerpt_window = 20
+          idx = -1
+          diffs.each { |diff|
+            idx += 1 # can't use each_with_index since I need idx in rescue
             excerpt_start = [(char_num - excerpt_window), 0].max
             excerpt_end = [(char_num + diff.last.length + excerpt_window), string_1.length].min
             excerpt = case diff.first
@@ -46,9 +48,28 @@ module Suspension
               char_num += diff.last.length
               line_num += diff.last.count("\n")
             end
-            r
-          rescue ArgumentError => e
+            deltas << r
+          }
+        rescue ArgumentError => e
+          if e.message.index('invalid byte sequence')
             # Handles invalid UTF-8 byte sequences in diff
+            # This is caused by two different multibyte characters at the
+            # same position where the first bytes are identical, and a
+            # subsequent one is different. DMP splits that multibyte char into
+            # separate bytes and thus creates an invalid UTF8 character.
+            # Here we re-combine the bytes into a valid UTF8 string, giving
+            # up some diff precision.
+
+            # We currently handle only one scenario where we have common prefix
+            # and different suffixes.
+            # Strategy: Remove common prefix, prepend it to the two different
+            # suffixes and use the combined strings as diffs.
+            common_prefix = diffs[idx].last
+            diffs[idx+1][1].prepend(common_prefix) # prepend to +1
+            diffs[idx+2][1].prepend(common_prefix) # prepend to +2
+            diffs.delete_at(idx) # delete the common prefix
+            retry
+          else
             valid_excerpt, valid_string = [excerpt, diff.last].map { |e|
               e.to_s.force_encoding('UTF-8') \
                     .encode('UTF-16', :invalid => :replace, :replace => '[invalid UTF-8 byte]') \
@@ -60,7 +81,7 @@ module Suspension
             $stderr.puts " - excerpt: #{ excerpt.inspect }"
             raise e
           end
-        }
+        end
         deltas = deltas.find_all { |e| 0 != e.first }
       end
     end
--- spec/string_comparer_spec.rb
+++ spec/string_comparer_spec.rb
@@ -15,25 +15,38 @@ module Suspension
         [
           'word word word word word word',
           'word word word added word word word',
-          [[1, "added ", "line 0", "word word word added word word"]]
+          [[1, "added ", "line 1", "word word word added word word"]]
         ],
         [
           'word word word deleted word word word',
           'word word word word word word',
-          [[-1, "deleted ", "line 0", "word word word deleted word word word"]]
+          [[-1, "deleted ", "line 1", "word word word deleted word word word"]]
         ],
         [
           'word word word xxxx word word word',
           'word word word yyyy word word word',
           [
-            [-1, "xxxx", "line 0", "word word word xxxx word word word"],
-            [1,  "yyyy", "line 0", "word word word yyyy word word word"]
+            [-1, "xxxx", "line 1", "word word word xxxx word word word"],
+            [1,  "yyyy", "line 1", "word word word yyyy word word word"]
           ]
         ],
         [
           "line1\nline2\nline3\nline4\nline5\nline6",
           "line1\nline2\nline3\nline4 added\nline5\nline6",
           [[1, " added", "line 4", "e1\nline2\nline3\nline4 added\nline5\n"]]
+        ],
+        [
+          'unicode chars — emdash',
+          'unicode chars emdash',
+          [[-1, "— ", "line 1", "unicode chars — emdash"]]
+        ],
+        [
+          'multibyte char alignment — one multibyte char is emdash, the other elipsis',
+          'multibyte char alignment … one multibyte char is emdash, the other elipsis',
+          [
+            [-1, "multibyte char alignment —", "line 1", "multibyte char alignment — one multibyte char i"],
+            [1, "multibyte char alignment …", "line 1", "yte char alignment … one multibyte char is emdash, the other elipsi"]
+          ]
         ],
       ].each do |(string_1, string_2, xpect)|
         it "handles #{ string_1.inspect } -> #{ string_2.inspect }" do
